6aecb13637b7205555c80b481ffe52ee02817c79
Stefano Ruggiu <steruggiu@gmail.com>
2022-02-23 15:05:33 +0000

    rimosso faker e aggiunto versione corretta, migrata la tabella

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+//il faker si utilizza in sostituzione dei dati reali quando questi ancora non sono disponibili.
+use Faker\Generator as Faker;
+use App\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
